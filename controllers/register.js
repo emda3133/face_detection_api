0d2529371e385fd6938512a8e4b30f1b97ed7013
Emily <emilydaykin@hotmail.com>
2021-03-02 12:27:37 +0100

    Fixing database connection

--- controllers/register.js
+++ controllers/register.js
@@ -30,8 +30,8 @@ const handleRegister = (req, res, db, bcrypt) => {
       .catch(trx.rollback)
     })
 
-    // .catch(err => res.status(400).json(err))  // the error "email already exists" is giving away info, so just say "unable to join"
-    .catch(err => res.status(400).json('Unable to register'))
+    .catch(err => res.status(400).json(err))  // the error "email already exists" is giving away info, so just say "unable to join"
+    // .catch(err => res.status(400).json('Unable to register'))
 }
 
 module.exports = {
